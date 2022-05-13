b8384980bd89ffd0c184b589a0ed2e753ddc2a10
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2022-05-13 19:35:07 +0100

    Update led-blinking.js

--- led-blinking.js
+++ led-blinking.js
@@ -1,16 +1,16 @@
 var Gpio = require("onoff").Gpio;
 var LED = new Gpio(4, "out");
-var blinkInterval = setInterval(blinkLED, 250);
+var blinkInterval = setInterval(blinkLED, 250); // Executes blinkLED function after every 250 miliseconds
 
 function blinkLED() {
   
   if (LED.readSync() === 0) {
     
-    LED.writeSync(1);
+    LED.writeSync(1); //Turn LED On
   
   } else {
     
-    LED.writeSync(0);
+    LED.writeSync(0); // Turn LED Off
   
   }
 
@@ -18,10 +18,10 @@ function blinkLED() {
 
 function endBlink() {
   
-  clearInterval(blinkInterval);
-  LED.writeSync(0);
-  LED.unexport();
+  clearInterval(blinkInterval)
+  LED.writeSync(0); // Turns off LED
+  LED.unexport(); // Frees resources after blinking has ended
 
 }
 
-setTimeout(endBlink, 10000);
+setTimeout(endBlink, 10000); // Stops blinking after 10 seconds
