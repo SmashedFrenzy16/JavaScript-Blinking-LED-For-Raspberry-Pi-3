e03e2e9807f739a851b1e802d8648f8d8660d1fe
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2022-05-13 19:02:16 +0100

    Update led-blinking.js

--- led-blinking.js
+++ led-blinking.js
@@ -1,3 +1,17 @@
 var Gpio = require("onoff").Gpio;
 var LED = new Gpio(4, "out");
-var blinkInterval = setInterval(blinLED, 250);
+var blinkInterval = setInterval(blinkLED, 250);
+
+function blinkLED() {
+  
+  if (LED.readSync() === 0) {
+    
+    LED.writeSync(1);
+  
+  } else {
+    
+    LED.writeSync(0);
+  
+  }
+
+}
